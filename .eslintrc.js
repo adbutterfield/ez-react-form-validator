731d0516316abda08ac95c12172e25587582771b
Adam Butterfield <adam.butterfield@ymail.com>
2020-03-31 17:50:06 +0900

    Setup project

---
+++ .eslintrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+  parserOptions: {
+    project: './tsconfig.json',
+    tsconfigRootDir: __dirname,
+  },
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+    'import/resolver': {
+      // use <root>/tsconfig.json
+      typescript: {
+        alwaysTryTypes: true, // always try to resolve types under `<roo/>@types` directory even it doesn't contain any source code, like `@types/unist`
+      },
+    },
+  },
+  extends: ['airbnb-typescript', 'react-app'],
+  rules: {
+    'arrow-body-style': 'off',
+    'react-hooks/exhaustive-deps': 'off',
+    'react/prop-types': 0,
+    'max-len': ['off'],
+    'spaced-comment': 0,
+    'react/jsx-boolean-value': 0,
+    'object-curly-newline': 0,
+    'react/jsx-one-expression-per-line': 0,
+    'no-irregular-whitespace': 0,
+    'arrow-parens': [
+      'error',
+      'always',
+    ],
+    'no-plusplus': [
+      'warn',
+      {
+        allowForLoopAfterthoughts: true,
+      },
+    ],
+    'no-param-reassign': ['error', { props: false }],
+  },
+};
