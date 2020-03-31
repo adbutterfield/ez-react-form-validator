1602cdb0b6050859077197b045c4b2830f46a3c3
Adam Butterfield <adam.butterfield@ymail.com>
2020-03-31 17:51:22 +0900

    Add package entrypoint

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import useFormValidator from './use-form-validator';
+import {
+  Validation,
+  ValidatorSetup,
+  ValidatorFn,
+  Field,
+  ErrorTypes,
+  ErrorMessages,
+  FormState,
+} from '../types';
+
+export default useFormValidator;
+
+export {
+  Validation,
+  ValidatorSetup,
+  ValidatorFn,
+  Field,
+  ErrorTypes,
+  ErrorMessages,
+  FormState,
+};
