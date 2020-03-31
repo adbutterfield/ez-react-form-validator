2b2fafae31b0c2e55a03c66f090ae8758578c71e
Adam Butterfield <adam.butterfield@ymail.com>
2020-03-31 17:50:26 +0900

    Add types

---
+++ types.d.ts
@@ -0,0 +1,54 @@
+export type Validation<T> = {
+  defaultValue?: T[keyof T];
+  required?: boolean;
+  pattern?: RegExp;
+  min?: number;
+  max?: number;
+  errors?: {
+    required?: string;
+    pattern?: string;
+    min?: string;
+    max?: string;
+  }
+};
+
+export type ValidatorSetup<T> = {
+  [K in keyof T]: Validation<T>;
+};
+
+export type ValidatorFn<T, K extends keyof T> = (value: T[K]) => { type: ErrorTypes, isValid: boolean };
+
+export type Field = {
+  touched: boolean;
+  dirty: boolean;
+  hasError: boolean;
+  showError: boolean;
+  isRequired: boolean;
+  isValid: boolean;
+  errors: string[];
+};
+
+export type ErrorTypes = 'required' | 'pattern' | 'min' | 'max';
+
+export type ErrorMessages = {
+  required: string;
+  pattern: string;
+  min: string;
+  max: string;
+};
+
+export type FormState<T> = {
+  values: {
+    [K in keyof T]: T[K];
+  };
+  fields: {
+    [K in keyof T]: Field;
+  };
+  validationRules: {
+    [K in keyof T]: ValidatorFn<T, K>[];
+  };
+  isValid: boolean;
+  errorMessages: {
+    [K in keyof T]: ErrorMessages;
+  }
+};
