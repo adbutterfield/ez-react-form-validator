3d7356d3cf6c11df107c96ca97539f4d337950b2
Adam Butterfield <adam.butterfield@ymail.com>
2020-04-07 19:39:27 +0900

    Setup formatting with prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,8 @@
 module.exports = {
   parserOptions: {
-    project: ['./tsconfig.json', './tsconfig.jest.json'],
-    tsconfigRootDir: __dirname,
+    ecmaVersion: 2015,
+    sourceType: 'module',
   },
-  settings: {
-    'import/parsers': {
-      '@typescript-eslint/parser': ['.ts', '.tsx'],
-    },
-    'import/resolver': {
-      // use <root>/tsconfig.json
-      typescript: {
-        alwaysTryTypes: true, // always try to resolve types under `<roo/>@types` directory even it doesn't contain any source code, like `@types/unist`
-      },
-    },
-  },
-  extends: ['airbnb-typescript', 'react-app'],
   rules: {
     'arrow-body-style': 'off',
     'react-hooks/exhaustive-deps': 'off',
@@ -25,10 +13,7 @@ module.exports = {
     'object-curly-newline': 0,
     'react/jsx-one-expression-per-line': 0,
     'no-irregular-whitespace': 0,
-    'arrow-parens': [
-      'error',
-      'always',
-    ],
+    'arrow-parens': ['error', 'always'],
     'no-plusplus': [
       'warn',
       {
--- jest.config.js
+++ jest.config.js
@@ -1,17 +1,17 @@
 module.exports = {
   transform: {
-    '^.+\\.tsx?$': 'ts-jest'
+    '^.+\\.tsx?$': 'ts-jest',
   },
   globals: {
     'ts-jest': {
       tsConfig: 'tsconfig.jest.json',
     },
   },
-  testRegex: '.+\.test\.tsx?$',
+  testRegex: '.+.test.tsx?$',
   moduleFileExtensions: ['ts', 'tsx', 'js'],
   // Indicates whether the coverage information should be collected while executing the test
   collectCoverage: true,
 
   // An array of glob patterns indicating a set of files for which coverage information should be collected
   collectCoverageFrom: ['src/*.ts'],
-}
+};
